7cdcbf33ad9e57b0decc6f3841c181ab5cac0e70
Certil Remy <certil@noukod.com>
2019-02-08 20:23:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ class BoardMember{
     veto(){
         return 'No, I must disagree'
       }
-      
+
       approve(){
         return 'You can do that!'
       }
@@ -24,7 +24,7 @@ class BoardMember{
         return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
       }
     }
-    
+
     class Ceo extends BoardMember {
       hireEmployee(){
         return "Welcome aboard!"
